32830f38f75f248e751b05e453bcaab4d71c36b5
Smithnour <96708309+Smithnour@users.noreply.github.com>
2026-06-01 00:31:33 +0100

    Update race.py

--- race.py
+++ race.py
@@ -1,1 +1,10 @@
+import random
 
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.speed = random.randint(40, 80)
+        self.distance = 0
+
+    def move(self):
+        self.distance += self.speed + random.randint(-5, 10)
